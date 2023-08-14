1e774cd9de7fd886ff7511e31242da9d1b3f075a
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-14 11:58:25 -0500

    MVP reached

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
+import { customerList } from "./customer.js";
 import { employeesOutput } from "./employees.js";
 
 const container = document.querySelector("#container");
+const customerContainer = document.querySelector("#customerContainer");
 
 const render = async () => {
   const employeeHTML = await employeesOutput();
-
+  const customerHTML = await customerList();
   container.innerHTML = employeeHTML;
+  customerContainer.innerHTML = customerHTML;
 };
 
 render();
